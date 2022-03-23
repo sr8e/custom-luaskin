d8d10a6990eb7df8a8e005d3b4bf8881d24bd6ad
sr8e <sr@mell0w-5phere.net>
2022-03-23 22:09:26 +0900

    add larger number font and replace #6

--- play/values.lua
+++ play/values.lua
@@ -18,6 +18,10 @@ local num_src={
     j = {
         x = 153, w = 37, h = 56,
         y = {pg = 0, y = 168}
+    },
+    l = {
+        x = 144, w = 18, h = 27,
+        y = {w = 43}
     }
 }
 local function value_resource(id, src_name, col, divx, digit, ref, padding, divy, cycle)
@@ -54,30 +58,30 @@ local values = {
 	value_resource("nowbpm", "p", "w", 11, 4, 160),
 	value_resource("maxbpm", "p", "w", 11, 4, 90),
 	-- time left
-	value_resource("timeleft-m", "n", "w", 10, 2, 163),
-	value_resource("timeleft-s", "n", "w", 10, 2, 164, 1),
+	value_resource("timeleft-m", "l", "w", 10, 2, 163),
+	value_resource("timeleft-s", "l", "w", 10, 2, 164, 1),
 	-- hi-speed
-	value_resource("hispeed", "n", "w", 10, 2, 310),
-	value_resource("hispeed-d", "n", "w", 10, 2, 311, 1),
+	value_resource("hispeed", "l", "w", 10, 2, 310),
+	value_resource("hispeed-d", "l", "w", 10, 2, 311, 1),
 	-- ?
-	value_resource("duration", "n", "w", 10, 4, 312),
+	value_resource("duration", "l", "w", 10, 4, 312),
 	-- gauge
 	value_resource("gaugevalue", "p", "w", 10, 3, 107),
 	value_resource("gaugevalue-d", "p", "w", 10, 1, 407),
 	-- score rate
-	value_resource("scorerate", "n", "w", 10, 3, 102),
-	value_resource("scorerate-d", "n", "w", 10, 2, 103, 1),
+	value_resource("scorerate", "l", "w", 10, 3, 102),
+	value_resource("scorerate-d", "l", "w", 10, 2, 103, 1),
     -- score value
 	value_resource("currentscore", "p", "w", 10, 5, 71),
 	value_resource("bestscore", "p", "w", 10, 5, 150),
 	value_resource("targetscore", "p", "w", 10, 5, 121),
 	-- notes
-	value_resource("totalnotes", "n", "w", 10, 5, 106),
+	value_resource("totalnotes", "l", "w", 10, 5, 106),
 	-- lanecover
-	value_resource("lanecover-value", "n", "w", 10, 3, 14),
-	value_resource("lanecover-duration", "n", "w", 10, 4, 312),
+	value_resource("lanecover-value", "l", "w", 10, 3, 14),
+	value_resource("lanecover-duration", "l", "w", 10, 4, 312),
 	--lift
-	value_resource("liftcover-value", "n", "w", 10, 3, 314),
+	value_resource("liftcover-value", "l", "w", 10, 3, 314),
 	--value_resource("liftcover-duration", "n", "w", 10, 4, 312),
 	-- combo
 	value_resource("combo-pg", "j", "pg", 10, 6, 75, 0, 3, 100),
@@ -160,24 +164,24 @@ local destination = {
     dst("minbpm", geometry.minbpm_x, geometry.bpm_y, "p"),
     dst("nowbpm", geometry.nowbpm_x, geometry.bpm_y, "p"),
     dst("maxbpm", geometry.maxbpm_x, geometry.bpm_y, "p"),
-    dst("timeleft-m", geometry.timeleft_m_x, geometry.timeleft_y, "n", 1.5),
-    dst("timeleft-s", geometry.timeleft_s_x, geometry.timeleft_y, "n", 1.5),
-    dst("hispeed",  geometry.hispeed_x, geometry.hispeed_y, "n", 1.5),
-    dst("hispeed-d", geometry.hispeed_d_x, geometry.hispeed_y, "n", 1.5),
+    dst("timeleft-m", geometry.timeleft_m_x, geometry.timeleft_y, "l"),
+    dst("timeleft-s", geometry.timeleft_s_x, geometry.timeleft_y, "l"),
+    dst("hispeed",  geometry.hispeed_x, geometry.hispeed_y, "l"),
+    dst("hispeed-d", geometry.hispeed_d_x, geometry.hispeed_y, "l"),
     dst("currentscore", geometry.currentscore_x, geometry.currentscore_y, "p", 1),
     dst("targetscore", geometry.targetscore_x, geometry.targetscore_y, "p", 1),
-    dst("totalnotes", geometry.totalnotes_x, geometry.totalnotes_y, "n", 1.5),
+    dst("totalnotes", geometry.totalnotes_x, geometry.totalnotes_y, "l"),
     dst("gaugevalue", geometry.gaugevalue_x, geometry.gaugevalue_y, "p", 1.6, 1.5),
-    dst("scorerate", geometry.scorerate_x, geometry.scorerate_y, "n", 1.5),
-    dst("scorerate-d", geometry.scorerate_d_x, geometry.scorerate_y, "n", 1.5),
+    dst("scorerate", geometry.scorerate_x, geometry.scorerate_y, "l"),
+    dst("scorerate-d", geometry.scorerate_d_x, geometry.scorerate_y, "l"),
 }
 
 local function set_lanecover_dst(x)
     append_all(destination, {
-        dst("lanecover-value", x + geometry.lanes_w / 3, geometry.resolution.y - 27, "n", 1.5, 1, {270}, 4),
-        dst("liftcover-value", x + geometry.lanes_w / 3, geometry.judge_line_y + 6, "n", 1.5, 1, {270}, 3),
-        dst("lanecover-duration", x + geometry.lanes_w * 2 / 3, geometry.resolution.y - 27, "n", 1.5, 1, {270}, 4),
-        dst("lanecover-duration", x + geometry.lanes_w * 2 / 3, geometry.judge_line_y + 6, "n", 1.5, 1, {270}, 3),
+        dst("lanecover-value", x + geometry.lanes_w / 3, geometry.resolution.y - 27, "l", 1, 1, {270}, 4),
+        dst("liftcover-value", x + geometry.lanes_w / 3, geometry.judge_line_y + 6, "l", 1, 1, {270}, 3),
+        dst("lanecover-duration", x + geometry.lanes_w * 2 / 3, geometry.resolution.y - 27, "l", 1, 1, {270}, 4),
+        dst("lanecover-duration", x + geometry.lanes_w * 2 / 3, geometry.judge_line_y + 6, "l", 1, 1, {270}, 3),
     })
 end
 
